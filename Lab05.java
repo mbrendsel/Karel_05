eb763ced451cabd11f74fe741d3aee35497d55ab
mbrendsel <monica_brendsel@asl.org>
2014-11-25 10:10:38 -0500

    Update Lab05.java

--- Lab05.java
+++ Lab05.java
@@ -10,13 +10,27 @@ import edu.fcps.karel2.Display;
 import edu.fcps.karel2.Robot;
 
 public class Lab05 {
-	
-	//TODO define a class method, runTheRace, that will accept a Racer as argument
-	//TODO implement the runTheRace method so that it shuttles the Racer as needed
 
-     public static void main(String[] args) {
-          Display.openWorld("maps/shuttle.map");
-          Display.setSize(10, 10);
-          //TODO Construct three Racers and pass each to "runTheRace", in sequence
+     public static void runTheRace(Racer arg) {
+         arg.shuttle(4, 7);
+         arg.shuttle(6, 5);
+         arg.shuttle(8, 3);
+         arg.move();
      }
+     
+     public static void main(String[] args) {   
+     
+            Display.openWorld("maps/shuttle.map");
+            Display.setSize(10, 10);
+            Display.setSpeed(10);
+            
+            Racer alex = new Racer(7);
+            Racer jordan = new Racer (4);
+            Racer jack = new Racer(1);
+            runTheRace(alex);
+            runTheRace(jordan);
+            runTheRace(jack);
+            
+   }  
+
 }
